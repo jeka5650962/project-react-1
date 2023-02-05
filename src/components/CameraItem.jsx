a2bb065c86416e1dfaecafbe62321c5eedda74f1
Evgeniy Salish <jeka5650962@gmail.com>
2023-02-05 22:09:52 +0300

    050223 2209

--- src/components/CameraItem.jsx
+++ src/components/CameraItem.jsx
@@ -1,17 +1,17 @@
-function CameraItem() {
+function CameraItem(props) {
     return (
         <div className="cameras__item item">
             <div className="item__body">
                 <div className="item__image">
-                    <img src="images/photos/photo-1.jpg" alt="Olympus OM-D E-M5"/>
+                    <img src={props.image} alt={props.title}/>
                 </div>
                 <div className="item__info">
                     <div className="item__title">
-                        Olympus OM-D E-M5
+                        {props.title}
                     </div>
                     <div className="item__price">
                         <span className="item__cost shortcut-style">
-                            28 000 ₽
+                            {props.price} ₽
                         </span>
                         <span className="item__new">
                             Новое
