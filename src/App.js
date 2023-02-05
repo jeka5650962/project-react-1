3de88c6592e2d999c74ea98b9b6ddb732c8a8dc7
Evgeniy Salish <jeka5650962@gmail.com>
2023-02-05 21:05:01 +0300

    050223 2104

--- src/App.js
+++ src/App.js
@@ -1,57 +1,5 @@
 import './App.css'
-
-function SideBar() {
-    return (
-        <div className="cameras__filter">
-            <div className="cameras__count">Товаров 143</div>
-            <h1 className="cameras__title">Камеры</h1>
-            <label htmlFor="min-price"
-                   className="cameras__shortcut cameras__shortcut_1 shortcut-style">Цена,
-                ₽</label>
-            <div className="cameras__price">
-                <input id="min-price" type="number" name="minimum-price" placeholder="0"
-                       className="cameras__price-input cameras__price-input_1"/>
-                <input id="max-price" type="number" name="maximum-price" placeholder="499000"
-                       className="cameras__price-input cameras__price-input_2"/>
-            </div>
-            <div className="cameras__shortcut cameras__shortcut_2 shortcut-style">Бренд</div>
-            <div className="cameras__brands">
-                <div className="cameras__brand">
-                    <label className="cameras__brand-label">
-                        <input type="checkbox" name="brand-canon" className="real-checkbox"/>
-                        <span className="custom-checkbox"></span>
-                        Canon
-                    </label>
-                </div>
-                <div className="cameras__brand">
-                    <label className="cameras__brand-label">
-                        <input type="checkbox" name="brand-nikon" className="real-checkbox"
-                               checked/>
-                        <span className="custom-checkbox"></span>
-                        Nikon
-                    </label>
-                </div>
-                <div className="cameras__brand">
-                    <label className="cameras__brand-label">
-                        <input type="checkbox" name="brand-sony" className="real-checkbox"
-                               disabled/>
-                        <span className="custom-checkbox"></span>
-                        Sony
-                    </label>
-                </div>
-                <div className="cameras__brand">
-                    <label className="cameras__brand-label">
-                        <input type="checkbox" name="brand-panasonic" className="real-checkbox"
-                               checked
-                               disabled/>
-                        <span className="custom-checkbox"></span>
-                        Olympus
-                    </label>
-                </div>
-            </div>
-        </div>
-    )
-}
+import SideBar from './components/SideBar'
 
 function App() {
     return (
@@ -68,13 +16,21 @@ function App() {
                                             <img src="images/photos/photo-1.jpg" alt="Olympus OM-D E-M5"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Olympus OM-D E-M5</div>
+                                            <div className="item__title">
+                                                Olympus OM-D E-M5
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">28 000 ₽</span>
-                                                <span className="item__new">Новое</span>
+                                                <span className="item__cost shortcut-style">
+                                                    28 000 ₽
+                                                </span>
+                                                <span className="item__new">
+                                                    Новое
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -86,12 +42,18 @@ function App() {
                                             <img src="images/photos/photo-2.jpg" alt="Sony Alpha ILCE-A7M2 A7 mark II"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Sony Alpha ILCE-A7M2 A7 mark II</div>
+                                            <div className="item__title">
+                                                Sony Alpha ILCE-A7M2 A7 mark II
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">118 000 ₽</span>
+                                                <span className="item__cost shortcut-style">
+                                                    118 000 ₽
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -103,12 +65,18 @@ function App() {
                                             <img src="images/photos/photo-3.jpg" alt="Fujifilm X-T20"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Fujifilm X-T20</div>
+                                            <div className="item__title">
+                                                Fujifilm X-T20
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">12 500 ₽</span>
+                                                <span className="item__cost shortcut-style">
+                                                    12 500 ₽
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -120,12 +88,18 @@ function App() {
                                             <img src="images/photos/photo-4.jpg" alt="Fujifilm X-T20"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Fujifilm X-T20</div>
+                                            <div className="item__title">
+                                                Fujifilm X-T20
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">15 000 ₽</span>
+                                                <span className="item__cost shortcut-style">
+                                                    15 000 ₽
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -133,17 +107,25 @@ function App() {
                                 </div>
                                 <div className="cameras__item item">
                                     <div className="item__body">
-                                        <div className="item__novelty">новинка</div>
+                                        <div className="item__novelty">
+                                            новинка
+                                        </div>
                                         <div className="item__image">
                                             <img src="images/photos/photo-5.jpg" alt="Nikon d61 Kit 70mm f/1.4"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Nikon d61 Kit 70mm f/1.4</div>
+                                            <div className="item__title">
+                                                Nikon d61 Kit 70mm f/1.4
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">19 000 ₽</span>
+                                                <span className="item__cost shortcut-style">
+                                                    19 000 ₽
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -151,18 +133,28 @@ function App() {
                                 </div>
                                 <div className="cameras__item item">
                                     <div className="item__body">
-                                        <div className="item__novelty">новинка</div>
+                                        <div className="item__novelty">
+                                            новинка
+                                        </div>
                                         <div className="item__image">
                                             <img src="images/photos/photo-6.jpg" alt="Nikon d61 Kit 70mm f/1.4"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Nikon d61 Kit 70mm f/1.4</div>
+                                            <div className="item__title">
+                                                Nikon d61 Kit 70mm f/1.4
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">96 000 ₽</span>
-                                                <span className="item__new">Новое</span>
+                                                <span className="item__cost shortcut-style">
+                                                    96 000 ₽
+                                                </span>
+                                                <span className="item__new">
+                                                    Новое
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -174,12 +166,18 @@ function App() {
                                             <img src="images/photos/photo-7.jpg" alt="Fujifilm X-A1"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Fujifilm X-A1</div>
+                                            <div className="item__title">
+                                                Fujifilm X-A1
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">19 000 ₽</span>
+                                                <span className="item__cost shortcut-style">
+                                                    19 000 ₽
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
@@ -191,13 +189,21 @@ function App() {
                                             <img src="images/photos/photo-8.jpg" alt="Fujifilm X10 Back"/>
                                         </div>
                                         <div className="item__info">
-                                            <div className="item__title">Fujifilm X10 Back</div>
+                                            <div className="item__title">
+                                                Fujifilm X10 Back
+                                            </div>
                                             <div className="item__price">
-                                                <span className="item__cost shortcut-style">96 000 ₽</span>
-                                                <span className="item__new">Новое</span>
+                                                <span className="item__cost shortcut-style">
+                                                    96 000 ₽
+                                                </span>
+                                                <span className="item__new">
+                                                    Новое
+                                                </span>
                                             </div>
                                             <div className="item__bottom">
-                                                <button className="item__btn">В корзину</button>
+                                                <button className="item__btn">
+                                                    В корзину
+                                                </button>
                                                 <div className="item__like"></div>
                                             </div>
                                         </div>
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,56 @@
+function SideBar() {
+    return (
+        <div className="cameras__filter">
+            <div className="cameras__count">
+                Товаров 143
+            </div>
+            <h1 className="cameras__title">
+                Камеры
+            </h1>
+            <label htmlFor="min-price" className="cameras__shortcut cameras__shortcut_1 shortcut-style">
+                Цена, ₽
+            </label>
+            <div className="cameras__price">
+                <input id="min-price" type="number" name="minimum-price" placeholder="0"
+                       className="cameras__price-input cameras__price-input_1"/>
+                <input id="max-price" type="number" name="maximum-price" placeholder="499000"
+                       className="cameras__price-input cameras__price-input_2"/>
+            </div>
+            <div className="cameras__shortcut cameras__shortcut_2 shortcut-style">
+                Бренд
+            </div>
+            <div className="cameras__brands">
+                <div className="cameras__brand">
+                    <label className="cameras__brand-label">
+                        <input type="checkbox" name="brand-canon" className="real-checkbox"/>
+                        <span className="custom-checkbox"/>
+                        Canon
+                    </label>
+                </div>
+                <div className="cameras__brand">
+                    <label className="cameras__brand-label">
+                        <input type="checkbox" name="brand-nikon" className="real-checkbox" checked/>
+                        <span className="custom-checkbox"/>
+                        Nikon
+                    </label>
+                </div>
+                <div className="cameras__brand">
+                    <label className="cameras__brand-label">
+                        <input type="checkbox" name="brand-sony" className="real-checkbox" disabled/>
+                        <span className="custom-checkbox"/>
+                        Sony
+                    </label>
+                </div>
+                <div className="cameras__brand">
+                    <label className="cameras__brand-label">
+                        <input type="checkbox" name="brand-panasonic" className="real-checkbox" checked disabled/>
+                        <span className="custom-checkbox"/>
+                        Olympus
+                    </label>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SideBar
