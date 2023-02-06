14ec7a3f749bae140b847ea7e84fe36d7af74c4e
Evgeniy Salish <jeka5650962@gmail.com>
2023-02-06 15:35:44 +0300

    060223 1535

--- src/App.js
+++ src/App.js
@@ -12,37 +12,37 @@ function App() {
                             <SideBar/>
                             <div className="cameras__items">
                                 <CameraItem
-                                    title="Olympus OM-D E-M5"
+                                    title={'Olympus OM-D E-M5'}
                                     price={28000}
-                                    image="images/photos/photo-1.jpg"/>
+                                    image={'images/photos/photo-1.jpg'}/>
                                 <CameraItem
-                                    title="Sony Alpha ILCE-A7M2 A7 mark II"
+                                    title={'Sony Alpha ILCE-A7M2 A7 mark II'}
                                     price={118000}
-                                    image="images/photos/photo-2.jpg"/>
+                                    image={'images/photos/photo-2.jpg'}/>
                                 <CameraItem
-                                    title="Fujifilm X-T20"
+                                    title={'Fujifilm X-T20'}
                                     price={12500}
-                                    image="images/photos/photo-3.jpg"/>
+                                    image={'images/photos/photo-3.jpg'}/>
                                 <CameraItem
-                                    title="Fujifilm X-T20"
+                                    title={'Fujifilm X-T20'}
                                     price={15000}
-                                    image="images/photos/photo-4.jpg"/>
+                                    image={'images/photos/photo-4.jpg'}/>
                                 <CameraItem
-                                    title="Nikon d61 Kit 70mm f/1.4"
+                                    title={'Nikon d61 Kit 70mm f/1.4'}
                                     price={19000}
-                                    image="images/photos/photo-5.jpg"/>
+                                    image={'images/photos/photo-5.jpg'}/>
                                 <CameraItem
-                                    title="Nikon d61 Kit 70mm f/1.4"
+                                    title={'Nikon d61 Kit 70mm f/1.4'}
                                     price={96000}
-                                    image="images/photos/photo-6.jpg"/>
+                                    image={'images/photos/photo-6.jpg'}/>
                                 <CameraItem
-                                    title="Fujifilm X-A1"
+                                    title={'Fujifilm X-A1'}
                                     price={19000}
-                                    image="images/photos/photo-7.jpg"/>
+                                    image={'images/photos/photo-7.jpg'}/>
                                 <CameraItem
-                                    title="Fujifilm X10 Back"
+                                    title={'Fujifilm X10 Back'}
                                     price={96000}
-                                    image="images/photos/photo-8.jpg"/>
+                                    image={'images/photos/photo-8.jpg'}/>
                             </div>
                         </div>
                     </div>
