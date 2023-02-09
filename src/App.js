9a231c922dd7cde3d8f91563b6c2d935081db781
Evgeniy Salish <jeka5650962@gmail.com>
2023-02-09 21:28:55 +0300

    090223 2128

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import CameraItem from './components/CameraItem'
 function App() {
 
     const cameras = [
-        {title: 'Olympus OM-D E-M5', price: 28000, image: 'images/photos/photo-1.jpg', like: false},
-        {title: 'Sony Alpha ILCE-A7M2 A7 mark II', price: 118000, image: 'images/photos/photo-2.jpg', like: false},
-        {title: 'Fujifilm X-T20', price: 12500, image: 'images/photos/photo-3.jpg', like: false},
-        {title: 'Fujifilm X-T20', price: 15000, image: 'images/photos/photo-4.jpg', like: false},
-        {title: 'Nikon d61 Kit 70mm f/1.4', price: 19000, image: 'images/photos/photo-5.jpg', like: false},
-        {title: 'Nikon d61 Kit 70mm f/1.4', price: 96000, image: 'images/photos/photo-6.jpg', like: false},
-        {title: 'Fujifilm X-A1', price: 19000, image: 'images/photos/photo-7.jpg', like: false},
-        {title: 'Fujifilm X10 Back', price: 96000, image: 'images/photos/photo-8.jpg', like: false},
+        {id: 0, title: 'Olympus OM-D E-M5', price: 28000, image: 'images/photos/photo-1.jpg', like: false},
+        {
+            id: 1,
+            title: 'Sony Alpha ILCE-A7M2 A7 mark II',
+            price: 118000,
+            image: 'images/photos/photo-2.jpg',
+            like: false
+        },
+        {id: 2, title: 'Fujifilm X-T20', price: 12500, image: 'images/photos/photo-3.jpg', like: false},
+        {id: 3, title: 'Fujifilm X-T20', price: 15000, image: 'images/photos/photo-4.jpg', like: false},
+        {id: 4, title: 'Nikon d61 Kit 70mm f/1.4', price: 19000, image: 'images/photos/photo-5.jpg', like: false},
+        {id: 5, title: 'Nikon d61 Kit 70mm f/1.4', price: 96000, image: 'images/photos/photo-6.jpg', like: false},
+        {id: 6, title: 'Fujifilm X-A1', price: 19000, image: 'images/photos/photo-7.jpg', like: false},
+        {id: 7, title: 'Fujifilm X10 Back', price: 96000, image: 'images/photos/photo-8.jpg', like: false},
     ]
 
     return (
@@ -25,10 +31,12 @@ function App() {
                             <div className="cameras__items">
                                 {
                                     cameras.map(item => (
-                                        <CameraItem title={item.title}
-                                                    price={item.price}
-                                                    image={item.image}
-                                                    like={item.like}
+                                        <CameraItem
+                                            key={item.id}
+                                            title={item.title}
+                                            price={item.price}
+                                            image={item.image}
+                                            like={item.like}
                                         />
                                     ))
                                 }
